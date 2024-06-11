34485942b324f53e34ca7b938c8e3c1b762a69ac
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-11 09:57:15 +0800

    Sequelize init
    - Install mysql2, sequelize, sequelize-cli packages.
    - Created database pet_project.
    - Generated model & migration files for Breeds table.

---
+++ models/breed.js
@@ -0,0 +1,19 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Breed extends Model {
+    static associate(models) {}
+  }
+  Breed.init(
+    {
+      name: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Breed',
+      tableName: 'Breeds',
+      underscored: true
+    }
+  )
+  return Breed
+}
