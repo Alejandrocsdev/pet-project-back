0c44f1ea3b7071c81c1ea22c2461e17256e1b0ca
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-11 18:44:50 +0800

    Add asyncError middleware to handle all error catch by a Promise

--- controllers/breeds.js
+++ controllers/breeds.js
@@ -1,16 +1,14 @@
 const { Breed } = require('../models')
 
+const { asyncError } = require('../middlewares')
+
 const { sucRes } = require('../utils/customResponse')
 
 class BreedsController {
-  getBreeds = async (req, res, next) => {
-    try {
-      const breeds = await Breed.findAll()
-      sucRes(res, 200, 'Get data successfully.', breeds)
-    } catch (err) {
-      next(err)
-    }
-  }
+  getBreeds = asyncError(async (req, res, next) => {
+    const breeds = await Breed.findAll()
+    sucRes(res, 200, 'Get data successfully.', breeds)
+  })
 }
 
 module.exports = new BreedsController()
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const defaultRoute = require('./defaultRoute')
+const asyncError = require('./asyncError')
 const globalError = require('./globalError')
 
-module.exports = { defaultRoute, globalError }
+module.exports = { defaultRoute, asyncError, globalError }
