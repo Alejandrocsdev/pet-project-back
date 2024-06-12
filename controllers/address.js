d8930ee915fe745deb4fdf2f6f8ee5435455abe4
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-12 19:38:49 +0800

    Add GET /address/roads & /roads/:roadId routes

--- controllers/address.js
+++ controllers/address.js
@@ -8,21 +8,27 @@ const Validator = require('../Validator')
 
 class AddressController extends Validator {
   getCities = asyncError(async (req, res, next) => {
-    const cities = await City.findAll({ include: [{ model: District, as: 'districts' }] })
+    const cities = await City.findAll({
+      include: [{ model: District, as: 'districts' }]
+    })
 
     sucRes(res, 200, 'Get all Cities table data successfully.', cities)
   })
 
   getCity = asyncError(async (req, res, next) => {
     const { cityId } = req.params
-    const city = await City.findByPk(cityId, { include: [{ model: District, as: 'districts' }] })
+    const city = await City.findByPk(cityId, {
+      include: [{ model: District, as: 'districts' }]
+    })
     this.validatePk(city)
 
     sucRes(res, 200, `Get Breeds table data from id ${cityId} successfully.`, city)
   })
 
   getDistricts = asyncError(async (req, res, next) => {
-    const districts = await District.findAll({ include: [{ model: Road, as: 'roads' }] })
+    const districts = await District.findAll({
+      include: [{ model: Road, as: 'roads' }]
+    })
 
     sucRes(res, 200, 'Get all Districts table data successfully.', districts)
   })
@@ -35,6 +41,20 @@ class AddressController extends Validator {
     this.validatePk(district)
 
     sucRes(res, 200, `Get Districts table data from id ${districtId} successfully.`, district)
+  })
+
+  getRoads = asyncError(async (req, res, next) => {
+    const roads = await Road.findAll()
+
+    sucRes(res, 200, 'Get all Roads table data successfully.', roads)
+  })
+
+  getRoad = asyncError(async (req, res, next) => {
+    const { roadId } = req.params
+    const road = await Road.findByPk(roadId)
+    this.validatePk(road)
+
+    sucRes(res, 200, `Get Roads table data from id ${roadId} successfully.`, road)
   })
 }
 
--- routes/address.js
+++ routes/address.js
@@ -7,14 +7,14 @@ const { checkId } = require('../middlewares')
 
 router.param('cityId', checkId)
 router.param('districtId', checkId)
-// router.param('roadId', checkId)
+router.param('roadId', checkId)
 
 router.get('/cities', addressController.getCities)
 router.get('/districts', addressController.getDistricts)
-// router.get('/roads', addressController.getRoads)
+router.get('/roads', addressController.getRoads)
 
 router.get('/cities/:cityId', addressController.getCity)
 router.get('/districts/:districtId', addressController.getDistrict)
-// router.get('/roads/:road', addressController.getRoad)
+router.get('/roads/:roadId', addressController.getRoad)
 
 module.exports = router
