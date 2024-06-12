be27a0f033fd6620642751cab5e43fdcd0047f58
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-12 19:33:40 +0800

    Add GET /address/districts & /districts/:districtId routes
    - Set up Districts & Roads table one to many relationship.

--- controllers/address.js
+++ controllers/address.js
@@ -1,4 +1,4 @@
-const { City, District } = require('../models')
+const { City, District, Road } = require('../models')
 
 const { asyncError } = require('../middlewares')
 
@@ -6,7 +6,7 @@ const { sucRes } = require('../utils/customResponse')
 
 const Validator = require('../Validator')
 
-class BreedsController extends Validator {
+class AddressController extends Validator {
   getCities = asyncError(async (req, res, next) => {
     const cities = await City.findAll({ include: [{ model: District, as: 'districts' }] })
 
@@ -20,6 +20,22 @@ class BreedsController extends Validator {
 
     sucRes(res, 200, `Get Breeds table data from id ${cityId} successfully.`, city)
   })
+
+  getDistricts = asyncError(async (req, res, next) => {
+    const districts = await District.findAll({ include: [{ model: Road, as: 'roads' }] })
+
+    sucRes(res, 200, 'Get all Districts table data successfully.', districts)
+  })
+
+  getDistrict = asyncError(async (req, res, next) => {
+    const { districtId } = req.params
+    const district = await District.findByPk(districtId, {
+      include: [{ model: Road, as: 'roads' }]
+    })
+    this.validatePk(district)
+
+    sucRes(res, 200, `Get Districts table data from id ${districtId} successfully.`, district)
+  })
 }
 
-module.exports = new BreedsController()
+module.exports = new AddressController()
--- routes/address.js
+++ routes/address.js
@@ -6,15 +6,15 @@ const { addressController } = require('../controllers')
 const { checkId } = require('../middlewares')
 
 router.param('cityId', checkId)
-// router.param('districtId', checkId)
+router.param('districtId', checkId)
 // router.param('roadId', checkId)
 
 router.get('/cities', addressController.getCities)
-// router.get('/districts', addressController.getDistricts)
+router.get('/districts', addressController.getDistricts)
 // router.get('/roads', addressController.getRoads)
 
 router.get('/cities/:cityId', addressController.getCity)
-// router.get('/districts/:districtId', addressController.getDistrict)
+router.get('/districts/:districtId', addressController.getDistrict)
 // router.get('/roads/:road', addressController.getRoad)
 
 module.exports = router
